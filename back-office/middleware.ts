2a7787a2b8c2ba9a12e0982450f86ba5014a6ed3
Ricardo Almeida <ricardo2675@proton.me>
2025-05-28 09:10:33 +0100

    Created middleware to manage public and private routes

---
+++ back-office/middleware.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(request: NextRequest) {
+    const publicPaths = ['/login', '/api/authentication/login'];
+    const isPublicPath = publicPaths.some((path) => request.nextUrl.pathname.startsWith(path));
+
+    if (isPublicPath) {
+        return NextResponse.next();
+    }
+
+    const token = request.cookies.get('authentication-token')?.value;
+
+    if (!token) {
+        return NextResponse.redirect(new URL('/login', request.url));
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         * - icon.svg and other icon files
+         */
+        '/((?!_next/static|_next/image|favicon.ico|icon.svg|.*\\.ico|.*\\.png|.*\\.svg).*)',
+    ],
+};
