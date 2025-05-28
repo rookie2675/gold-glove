f677b227820d3c650bdb9a0d31d456c95850a9ee
Ricardo Almeida <ricardo2675@proton.me>
2025-05-28 10:01:24 +0100

    Fixed missing Toaster component from body children

--- back-office/app/layout.tsx
+++ back-office/app/layout.tsx
@@ -3,8 +3,11 @@ import { Syne } from 'next/font/google';
 import './globals.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import { Toaster } from 'sonner';
 
-const syne = Syne({});
+const syne = Syne({
+    subsets: ['latin'],
+});
 
 export const metadata: Metadata = {
     title: 'Gold Glove',
@@ -18,6 +21,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
                 <Header />
                 <main>{children}</main>
                 <Footer />
+                <Toaster />
             </body>
         </html>
     );
